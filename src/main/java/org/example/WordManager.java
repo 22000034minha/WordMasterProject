512e3efe17e61f15b9a8e3c884a352bba0ab1f70
22000034 <minha20@handong.ac.kr>
2022-09-07 10:24:50 +0900

    create project

---
+++ src/main/java/org/example/WordManager.java
@@ -0,0 +1,19 @@
+// manage crud works according to users
+
+package org.example;
+
+public class WordManager {
+    public void start() {
+        System.out.print("********************\n" +
+                "1. 모든 단어 보기\n" +
+                "2. 수준별 단어 보기\n" +
+                "3. 단어 검색\n" +
+                "4. 단어 추가\n" +
+                "5. 단어 수정\n" +
+                "6. 단어 삭제\n" +
+                "7. 파일 저장\n" +
+                "0. 나가기\n" +
+                "********************\n" +
+                "=> 원하는메뉴는? ");
+    }
+}
